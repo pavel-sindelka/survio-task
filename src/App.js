7f739b43a01a92ac836acec0ad0bf9c4396321b6
Pavel Šindelka <sindelka@survio.cz>
2020-06-29 19:07:00 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="square relative"/>
+      <div className="square absolute"/>
+      <div className="square fixed"/>
+    </div>
+  );
+}
+
+export default App;
